5487e79d3e4457a071c9a7a406d952a03e3e45eb
ana paula <anapaulamak@gmail.com>
2019-11-17 11:55:42 -0300

    add menu

--- src/pages/Register.page.jsx
+++ src/pages/Register.page.jsx
@@ -1,63 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DriverList } from '../components/ListDrivers/ListDrivers.component';
 import { AddDriver } from '../components/AddDriver/AddDriver.component';
 import { EditDriver } from '../components/EditDriver/EditDriver.component';
 import './Register.styles.scss';
 
-class RegisterPage extends React.Component {
+function RegisterPage () {
+  const [driver, setDriver] = useState({
+    id: null,
+    driverId: 1,
+    driverName: '',
+    driverPhone: '',
+    driverBirth: '',
+    driverCnh: '',
+    driverCnhType: '',
+    driverCpf:'',
+    status: false,
+    driverItem: {},
+    driverItems: [
+      {
+        driverId:'1',
+        driverName: 'Ana Paula M. Aoyama Kajihara',
+        driverPhone: '11 986458116',
+        driverBirth: '05-08-1988',
+        driverCnh: '111111111',
+        driverCnhType: 'A',
+        driverCpf:'22222222222',
+      }
+    ],
+    editing: false,
+    page:0
+  })
 
-  constructor() {
-    super();
-    this.state = {
-      id: null,
-      driverId: 1,
-      driverName: '',
-      driverPhone: '',
-      driverBirth: '',
-      driverCnh: '',
-      driverCnhType: '',
-      driverCpf:'',
-      status: false,
-      driverItem: {},
-      driverItems: [],
-      editing: false
-    };
-    
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.deleteDriver= this.deleteDriver.bind(this);
-    this.addDriver = this.addDriver.bind(this);
-    this.editDriver = this.editDriver.bind(this);
-    this.setEditing = this.setEditing.bind(this);
-    this.updateDriver = this.updateDriver.bind(this);
+  const handleInputChange = (event) => {
+    setDriver({ ...driver, [event.target.name]: event.target.value });
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
- 
-    this.setState({
-      [name]:value
-    })
-  }
-
-  addDriver(event){
+  const addDriver = (event) => {
     event.preventDefault()
 
-    if (!this.state.driverName) return;
+    if (!driver.driverName) return;
     const driverItem = {
-      id: this.state.driverItems.length + 1,
-      driverId: this.state.driverItems.length + 1,
-      driverName: this.state.driverName,
-      driverPhone: this.state.driverPhone,
-      driverBirth: this.state.driverBirth,
-      driverCnh: this.state.driverCnh,
-      driverCnhType: this.state.driverCnhType,
-      driverCpf: this.state.driverCpf,
-      status: this.state.status
+      id: driver.driverItems.length + 1,
+      driverId: driver.driverItems.length + 1,
+      driverName: driver.driverName,
+      driverPhone: driver.driverPhone,
+      driverBirth: driver.driverBirth,
+      driverCnh: driver.driverCnh,
+      driverCnhType: driver.driverCnhType,
+      driverCpf: driver.driverCpf,
+      status: driver.status
     };
-
-    this.setState({
+    setDriver({
+      ...driver,
       driverName: '',
       driverPhone: '',
       driverBirth: '',
@@ -65,49 +59,46 @@ class RegisterPage extends React.Component {
       driverCnhType: '',
       driverCpf:'',
       driverItem: driverItem,
-      driverItems: [...this.state.driverItems, driverItem]
+      driverItems: [...driver.driverItems, driverItem]
     })
-    console.log(this.state.driverItems);
   }
 
-  deleteDriver(driverId) {
-    const driverItems = this.state.driverItems.filter( item => item.driverId !== driverId );
-    this.setState({driverItems: driverItems});
-    if(this.state.editing === true) {
+  const deleteDriver = (driverId) => {
+    const driverItems = driver.driverItems.filter( item => item.driverId !== driverId );
+    setDriver({...driver, driverItems: driverItems});
+    if(driver.editing === true) {
       window.location.reload();
     }
   }
 
-  editDriver(driverItem) {
-    this.setEditing(true);
-    this.setState({
-      driverName:driverItem.driverName,
-      driverPhone:driverItem.driverPhone,
-      driverBirth:driverItem.driverBirth,
-      driverCnh:driverItem.driverCnh,
-      driverCnhType:driverItem.driverCnhType,
-      driverCpf:driverItem.driverCpf,
-      driverItem: driverItem
-    });
-    console.log(driverItem);
-  }
-
-  setEditing(value) {
-    this.setState({
-      editing: value
+  const editDriver = (driverItem) => {
+    setEditing(true);
+    setDriver({
+      ...driver,
+      driverName: driverItem.driverName,
+      driverPhone: driverItem.driverPhone,
+      driverBirth: driverItem.driverBirth,
+      driverCnh: driverItem.driverCnh,
+      driverCnhType: driverItem.driverCnhType,
+      driverCpf: driverItem.driverCpf,
+      driverItem: driverItem,
     })
   }
+  
+  const setEditing = (value) => {
+    setDriver({...driver, editing: value })
+  }
 
-  updateDriver(event) {
+  const updateDriver = (event) => {
     event.preventDefault();
-    const updatedName = this.state.driverName;
-    const updatedPhone = this.state.driverPhone;
-    const updatedBirth = this.state.driverBirth;
-    const updatedCnh = this.state.driverCnh;
-    const updatedCnhType = this.state.driverCnhType;
-    const updatedCpf = this.state.driverCpf;
+    const updatedName = driver.driverName;
+    const updatedPhone = driver.driverPhone;
+    const updatedBirth = driver.driverBirth;
+    const updatedCnh = driver.driverCnh;
+    const updatedCnhType = driver.driverCnhType;
+    const updatedCpf = driver.driverCpf;
 
-    const updatedDriverInfos = Object.assign({}, this.state.driverItem, {
+    const updatedDriverInfos = Object.assign({}, driver.driverItem, {
       driverName: updatedName,
       driverPhone: updatedPhone,
       driverBirth: updatedBirth,
@@ -115,55 +106,82 @@ class RegisterPage extends React.Component {
       driverCnhType: updatedCnhType,
       driverCpf: updatedCpf,
     })
-    const driverItems = this.state.driverItems.map((driverItem) => (driverItem.id === this.state.driverItem.id ? updatedDriverInfos : driverItem));
-    this.setState({ driverName:'', driverPhone: '', driverBirth: '', driverCnh: '', driverCnhType: '', driverCpf: '', driverItems: driverItems});
+    const driverItems = driver.driverItems.map((driverItem) => (driverItem.id === driver.driverItem.id ? updatedDriverInfos : driverItem));
+    setDriver({ 
+      ...driver,
+      driverName:'',
+      driverPhone: '',
+      driverBirth: '',
+      driverCnh: '',
+      driverCnhType: '',
+      driverCpf: '',
+      driverItems: driverItems
+    });
   }
 
-  render() {
-    const { driverItems, editing } = this.state;
-      return (
-        <div className="App">
-          <h1>Cadastrar motorista</h1>
+  const handleTab = index => event => {
+    setDriver({...driver, page:index})
+  }
+
+  const handlePage = () => {
+    switch(driver.page) {
+      case 0:
+        return (
           <div className="App-main">
           { 
-            editing  ? (
+            driver.editing ? (
             <EditDriver 
-             driverName={this.state.driverName}
-             driverPhone={this.state.driverPhone}
-             driverBirth={this.state.driverBirth}
-             driverCnh={this.state.driverCnh}
-             driverCnhType={this.state.driverCnhType}
-             driverCpf={this.state.driverCpf}
-             handleInputChange={this.handleInputChange}
-             setEditing={this.setEditing}
-             updateDriver={this.updateDriver}
+              driverName={ driver.driverName }
+              driverPhone={ driver.driverPhone }
+              driverBirth={ driver.driverBirth }
+              driverCnh={ driver.driverCnh }
+              driverCnhType={ driver.driverCnhType }
+              driverCpf={ driver.driverCpf }
+              handleInputChange={ handleInputChange }
+              setEditing={ setEditing }
+              updateDriver={ updateDriver }
             />
             ) : (
             <AddDriver
-              driverName={this.state.driverName}
-              driverPhone={this.state.driverPhone}
-              driverBirth={this.state.driverBirth}
-              driverCnh={this.state.driverCnh}
-              driverCnhType={this.state.driverCnhType}
-              driverCpf={this.state.driverCpf}
-              handleInputChange={this.handleInputChange} 
-              addDriver={this.addDriver}
+              driverName={ driver.driverName }
+              driverPhone={ driver.driverPhone }
+              driverBirth={ driver.driverBirth }
+              driverCnh={ driver.driverCnh }
+              driverCnhType={ driver.driverCnhType }
+              driverCpf={ driver.driverCpf }
+              handleInputChange={ handleInputChange } 
+              addDriver={ addDriver }
             />
             )
           }
           </div>
-
-          <h1>Motoristas cadastrados</h1>
+        );
+      case 1:
+        return (
           <div className="App-main">
-            <DriverList 
-              driverItems= {driverItems} 
-              deleteDriver={this.deleteDriver}
-              editDriver={this.editDriver}
-            />
-          </div>
-
+          <DriverList 
+            driverItems= { driver.driverItems } 
+            deleteDriver={ deleteDriver }
+            editDriver={ editDriver }
+          />
         </div>
-      );
+        );
+      default:
+      break;
     }
+  }
+
+  return (
+    <div className="App">
+      <ul>
+        <li onClick={handleTab(0)}>Cadastro</li>
+        <li onClick={handleTab(1)}>Lista</li>
+      </ul>
+
+      <div>
+        { handlePage() }
+      </div>
+    </div>
+  );
 }
 export default RegisterPage;
